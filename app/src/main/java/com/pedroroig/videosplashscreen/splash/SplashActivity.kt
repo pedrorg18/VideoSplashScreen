5f1dbd11319344ec6dd3081d6c7f410d1965bbba
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-11-06 16:13:34 +0100

    show video at splash screen, when finished go to main screen

--- app/src/main/java/com/pedroroig/videosplashscreen/splash/SplashActivity.kt
+++ app/src/main/java/com/pedroroig/videosplashscreen/splash/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.pedroroig.videosplashscreen.splash
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pedroroig.videosplashscreen.MainActivity
 import com.pedroroig.videosplashscreen.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -12,11 +15,31 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        hideSystemUI()
+
+        setupVideo()
 
     }
 
+    private fun setupVideo() {
+        val video = Uri.parse("android.resource://" + packageName + "/" + R.raw.demo)
+        videoView.setVideoURI(video)
+
+        videoView.setOnCompletionListener { goToMainActivity() }
+
+        videoView.start()
+    }
 
     private fun goToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
